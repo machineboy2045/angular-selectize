c8d97dbaa6a583861c751f16c86b9c745a063e31
Jussi Ruokomäki <jussi.ruokomaki@takamaki.fi>
2022-06-03 13:32:55 +0300

    Fixes auto close after second selection

--- dist/angular-selectize.js
+++ dist/angular-selectize.js
@@ -55,7 +55,7 @@ angular.module('selectize', []).value('selectizeConfig', {}).directive("selectiz
         selectize.$control.toggleClass('ng-pristine', modelCtrl.$pristine);
 
         if (settings.maxItems !== 1 && !angular.equals(selectize.items, scope.ngModel) ||
-          selectize.items[0] != scope.ngModel) { // If maxItems is 1, items is an array and ngModel is a string
+          settings.maxItems === 1 && selectize.items[0] != scope.ngModel) { // If maxItems is 1, items is an array and ngModel is a string
           selectize.setValue(scope.ngModel, true);
         }
       }
